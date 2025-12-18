4cd5d33cff934223a6e6b4ab0a8457e4dcef91af
JM Mac <johnmarkmancol@gmail.com>
2025-12-19 00:12:16 +0800

    featured section

---
+++ app/components/FeaturedProject.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import type { ButtonProps } from "@nuxt/ui";
+
+const links = ref<ButtonProps[]>([
+  {
+    label: "Check it out",
+    color: "primary",
+    trailingIcon: "i-lucide-arrow-right",
+    to: "http://shop.juanmarcus.com",
+    target: "_blank",
+  },
+]);
+</script>
+
+<template>
+  <UPageCTA
+    title="Ecommerce Admin Application"
+    description="A personal project build with Laravel 12 and Nuxt 4. This project is still in development."
+    orientation="horizontal"
+    :links="links"
+  >
+    <img
+      src="../../ecommerce.png"
+      width="320"
+      height="364"
+      alt="Illustration"
+      class="w-full rounded-lg"
+    />
+  </UPageCTA>
+</template>
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,4 +1,6 @@
 <script setup lang="ts">
+import FeaturedProject from "~/components/FeaturedProject.vue";
+import Projects from "~/components/Projects.vue";
 import WorkExperienceVue from "~/components/WorkExperience.vue";
 </script>
 
@@ -32,92 +34,13 @@ import WorkExperienceVue from "~/components/WorkExperience.vue";
           <WorkExperienceVue />
         </div>
         <div class="flex-1">
-          <p class="uppercase text-xs font-bold mb-4">
+          <p class="uppercase text-xs font-bold mb-4">Featured Project</p>
+          <FeaturedProject />
+          <p class="uppercase text-xs font-bold mb-4 mt-8">
             Projects Collaborated<br />
             Through the Years
           </p>
-          <div class="grid xs:grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
-            <UPageCard
-              title="Capfrance Vacances"
-              description="Built the project from the ground up using Laravel and Vue.js, serving as a full-stack developer throughout its development."
-              orientation="vertical"
-              spotlight
-              spotlight-color="primary"
-            >
-              <img
-                src="/capfrance.png"
-                alt="Capfrance"
-                class="rounded-lg w-full"
-              />
-              <div class="flex flex-wrap gap-1 h-fit">
-                <UBadge label="Vue2" variant="outline" />
-                <UBadge label="Laravel 9" variant="outline" />
-              </div>
-            </UPageCard>
-            <UPageCard
-              title="NIPT Applications and API"
-              description="Contributed to a multi-portal project by setting up the My Page application with Nuxt.js and developing a Laravel API migrated from CodeIgniter."
-              orientation="vertical"
-              spotlight
-              spotlight-color="primary"
-            >
-              <img src="/nipt.png" alt="NIPT" class="rounded-lg w-full" />
-              <div class="flex flex-wrap gap-1 h-fit">
-                <UBadge label="Vue2" variant="outline" />
-                <UBadge label="NuxtJS" variant="outline" />
-                <UBadge label="Laravel 10" variant="outline" />
-              </div>
-            </UPageCard>
-            <UPageCard
-              title="Allworld Communications"
-              description="This project was built entirely on PrestaShop, leveraging its built-in e-commerce features. My responsibilities included developing the custom theme and implementing the front-end components."
-              orientation="vertical"
-              spotlight
-              spotlight-color="primary"
-            >
-              <img
-                src="/allworld.png"
-                alt="Tailwind CSS"
-                class="rounded-lg w-full"
-              />
-              <div class="flex flex-wrap gap-1 h-fit">
-                <UBadge label="Prestashop" variant="outline" />
-                <UBadge label="Smarty" variant="outline" />
-              </div>
-            </UPageCard>
-            <UPageCard
-              title="Xode"
-              description="This project was developed collaboratively by a team of developers. The main website is built using WordPress, while my contribution focused on the Blockscanner page, which was developed with Next.js and integrates Polkadot APIs to retrieve and display blockchain data."
-              orientation="vertical"
-              spotlight
-              spotlight-color="primary"
-            >
-              <img
-                src="/xode.png"
-                alt="Tailwind CSS"
-                class="rounded-lg w-full"
-              />
-              <div class="flex flex-wrap gap-1 h-fit">
-                <UBadge label="ReactJS" variant="outline" />
-              </div>
-            </UPageCard>
-            <UPageCard
-              title="Multipay X API"
-              description="Developed the project’s APIs using NestJS, consisting of three distinct APIs, each with its own specific responsibilities, working collaboratively to deliver the overall system functionality."
-              orientation="vertical"
-              spotlight
-              spotlight-color="primary"
-            >
-              <img
-                src="/multipay-x.png"
-                alt="Tailwind CSS"
-                class="rounded-lg w-full"
-              />
-              <div class="flex flex-wrap gap-1 h-fit">
-                <UBadge label="NestJS" variant="outline" />
-              </div>
-            </UPageCard>
-          </div>
+          <Projects />
         </div>
       </div>
     </UPageSection>
