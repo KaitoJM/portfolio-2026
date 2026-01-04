2968c44ee5e410a75db150bdb8af9fc5a73f7a2a
JM Mac <johnmarkmancol@gmail.com>
2026-01-04 17:38:17 +0800

    refactor: feature section

--- app/components/FeaturedProject.vue
+++ app/components/FeaturedProject.vue
@@ -3,29 +3,39 @@ import type { ButtonProps } from "@nuxt/ui";
 
 const links = ref<ButtonProps[]>([
   {
-    label: "Check it out",
+    label: "Visit Shop",
     color: "primary",
     trailingIcon: "i-lucide-arrow-right",
     to: "http://shop.juanmarcus.com",
     target: "_blank",
   },
+  {
+    label: "Visit Backoffice",
+    color: "primary",
+    trailingIcon: "i-lucide-arrow-right",
+    to: "http://shopadmin.juanmarcus.com",
+    target: "_blank",
+  },
 ]);
+
+const items = ["/shop.juanmarcus.com.png", "/shopadmin.juanmarcus.com.png"];
 </script>
 
 <template>
   <UPageCTA
-    title="Ecommerce Admin Application"
+    title="Ecommerce Web Application"
     description="A personal project build with Laravel 12 and Nuxt 4. This project is still in development."
     orientation="horizontal"
     :links="links"
     variant="soft"
   >
-    <img
-      src="/ecommerce.png"
-      width="320"
-      height="364"
-      alt="Illustration"
-      class="w-full rounded-lg"
-    />
+    <UCarousel
+      v-slot="{ item }"
+      dots
+      :items="items"
+      class="w-full max-w-xs mx-auto"
+    >
+      <img :src="item" width="320" height="364" class="rounded-lg" />
+    </UCarousel>
   </UPageCTA>
 </template>
