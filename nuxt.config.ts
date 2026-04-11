f2d028a5a48441bcbd77688212c18617752993d3
JM Mac <johnmarkmancol@gmail.com>
2026-04-11 16:41:37 +0800

    chore: install vercel analytics

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,13 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   ssr: true,
-  modules: ["@nuxt/eslint", "@nuxt/ui"],
+  modules: ["@nuxt/eslint", "@nuxt/ui", "@vercel/analytics"],
 
   runtimeConfig: {
     public: {
       bloggerApiKey: import.meta.env.API_KEY,
-      blogIds: import.meta.env.BLOG_IDS ?? import.meta.env.BLOG_ID ?? ""
-    }
+      blogIds: import.meta.env.BLOG_IDS ?? import.meta.env.BLOG_ID ?? "",
+    },
   },
 
   devtools: {
